1588f935e201feab86863fe701f7524b071958a1
HafizJarot <hafizalexandria@gmail.com>
2020-06-01 20:09:15 +0700

    api register

--- app/Http/Controllers/Api/Auth/RegisterController.php
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -17,18 +17,18 @@ class RegisterController extends Controller
         $this->middleware('guest:user');
     }
 
-    public function register(Request $request){
+    public function registerPenyewa(Request $request){
 
         $this->validate($request,[
-           'email' => 'required|email|unique:users',
-           'password' => 'required'
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
         ]);
 
         $user = new User();
         $user->nama = $request->nama;
-        $user->nama_perusahaan = $request->nama_perusahaan;
         $user->email = $request->email;
-        $user->role = $request->role;
+        $user->role = false;
         $user->password = Hash::make($request->password);
         $user->api_token = Str::random(80);
         $user->save();
@@ -39,6 +39,32 @@ class RegisterController extends Controller
            'message' => $message,
            'status' => true,
            'data' => $user
+        ]);
+
+    }
+
+    public function registerPemilik(Request $request){
+
+        $this->validate($request,[
+            'nama_perusahaan' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+        ]);
+
+        $user = new User();
+        $user->nama_perusahaan = $request->nama_perusahaan;
+        $user->email = $request->email;
+        $user->role = true;
+        $user->password = Hash::make($request->password);
+        $user->api_token = Str::random(80);
+        $user->save();
+        $user->sendApiEmailVerificationNotification();
+        $message = "Cek Email Anda, Verifikasi Dahulu";
+
+        return response()->json([
+            'message' => $message,
+            'status' => true,
+            'data' => $user
         ]);
 
     }
