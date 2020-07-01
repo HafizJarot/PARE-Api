aa0b125861a6ee1e7acb12ca7574312dc419c3c0
HafizJarot <hafizalexandria@gmail.com>
2020-07-01 15:50:17 +0700

    PARE

--- resources/views/templates/partials/_navbar.blade.php
+++ resources/views/templates/partials/_navbar.blade.php
@@ -2,7 +2,7 @@
     <!-- Logo -->
     <a href="index.html" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>F</b></span>
+        <span class="logo-mini"><b></b></span>
         <!-- logo for regular state and mobile devices -->
         <span class="logo-lg"><b>Super</b>Admin</span>
     </a>
