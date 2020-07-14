53120cb1b35a9c9d226a7916eaba4d97f6957ca8
HafizJarot <hafizalexandria@gmail.com>
2020-07-14 20:55:30 +0700

    confirm

--- app/Http/Controllers/superadmin/NotifikasiPemilikReklameController.php
+++ app/Http/Controllers/superadmin/NotifikasiPemilikReklameController.php
@@ -23,73 +23,16 @@ class NotifikasiPemilikReklameController extends Controller
 
     public function index()
     {
-        $notifs = User::all()->where('status','0');
+        $notifs = User::where('role', true)->where('status','0')->get();
         return view('pages.admin.notif.notif', compact('notifs'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update($id)
     {
         $notif= User::find($id);
         $notif->update(['status' => '1']);
         return redirect()->route('notif.index');
     }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
